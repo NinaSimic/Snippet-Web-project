8638dec5d53ef07ebd09f7c909528d0ba9b9e70e
NinaSimic <ninasimicns@gmail.com>
2017-09-10 01:34:04 +0200

    Final commit: Dodat admin

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,6 +13,15 @@ INSERT INTO language(id,name) values(3,'JavaScript');
 --usernmae: admin
 INSERT INTO user( id, username, password, firstname, lastname, phone, email, address, anumber, city, country, image, role, authority_id, status) values(1, 'admin', '$2a$10$2nUwyYdXRxhKio6aVrwsVOXlUqSs9XnsIndPiyT.3AphhvZ/UYBta', 'Nina', 'Simic', '0641789544', 'ninasimicns@gmail.com', 'Dusana Danilovica','7', 'Novi Sad', 'Serbia', '/user-images/user_137_nina.jpg', 'ADMIN', 1, 'APPROVED');
 
+--ADMIN2
+--password: a
+--username: a
+INSERT INTO user( id, username, password, firstname, lastname, phone, email, address, anumber, city, country, image, role, authority_id, status) values(3, 'a', '$2a$10$KTYsWArMYDgUkVxs8QRW6OQzsKRjOIOmC/eXrKZMk2z7eccXLJHHW', 'Danijela', 'Damjanovic', '0607244994', 'dada@gmail.com', 'Narodnog Fronta','23a', 'Novi Sad', 'Serbia', '/user-images/user_137_nina.jpg', 'ADMIN', 1, 'APPROVED');
+
+
+
+
+
 -- USER
 --username: ver
 --password: ver
